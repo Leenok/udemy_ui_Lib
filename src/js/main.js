344bdaedda146119f45f44654a585efcd6f97464
Lenok <eseredich8@gmail.com>
2024-10-28 23:17:36 +0300

    feat: step 2

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,12 @@
 import './lib/lib';
 
-$('.one').hide().show();
+// $('.one').hide().show();
 
+// $('.active').addClass('hello', 'add');
+// $('.active').toggleClass('.active');
+$('.active').on('click', sayHello);
+$('button').on('click', sayHello);
+
+function sayHello(){
+    console.log('Hello');
+}
